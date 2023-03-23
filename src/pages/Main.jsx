c108cc39d2080e766535da5c32276de1237174cb
zionimo <zionimobu@gmail.com>
2023-03-23 15:31:05 +0900

    장애물 움직임 기능

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,33 +10,74 @@ const Main = () => {
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight); // (0,0)위치의 좌표에 100x100사이즈 네모를 그려달라
 
     // 러너
-    var mario = {
+    const mario = {
       x: 10,
       y: 200,
       width: 40,
       height: 50,
+      draw() {
+        // 러너 시각화
+        ctx.fillStyle = "red";
+        ctx.fillRect(mario.x, mario.y, mario.width, mario.height);
+      },
     };
+    mario.draw();
 
     // 장애물
-    const flower = {
-      x: 500,
-      y: 225,
-      width: 20,
-      height: 30,
-    };
+    class Flower {
+      constructor() {
+        this.x = 500;
+        this.y = 220;
+        this.width = 20;
+        this.height = 30;
+      }
+      draw() {
+        // 장애물 시각화
+        ctx.fillStyle = "black";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    }
+    var flower = new Flower();
 
-    const draw = () => {
-      ctx.fillStyle = "red";
-      ctx.fillRect(mario.x, mario.y, mario.width, mario.height);
+    const timer = 0;
+    const flowerArr = [];
 
-      ctx.fillStyle = "black";
-      ctx.fillRect(flower.x, flower.y, flower.width, flower.height);
-    };
+    // 1초마다 실행할 프레임 루프
     const loop = () => {
-      draw();
+      window.requestAnimationFrame(loop);
+      // 새롭게 그려진 장애물 이전의 장애물들 지우기
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      // 120프레임마다 장애물을 하나씩 생성
+      if (timer % 120 === 0) {
+        var flower = new Flower();
+        // 생선된 장애물을 배열에 추가하기
+        flowerArr.push(flower);
+      }
+
+      // 배열에 있는 모든 것을 다 draw()하기
+      flowerArr.forEach((a, i, o) => {
+        // 데이터가 쌓이지 않게 장애물의 x좌표가 0미만이면 제거
+        if (a.x < 0) {
+          o.splice(i, 1);
+        }
+        // a.x--;
+        a.draw();
+      });
     };
 
-    window.requestAnimationFrame(loop);
+    loop();
+
+    // // 시각화
+    // const draw = () => {
+    //   // 러너 시각화
+    //   ctx.fillStyle = "red";
+    //   ctx.fillRect(mario.x, mario.y, mario.width, mario.height);
+
+    //   // 장애물 시각화
+    //   ctx.fillStyle = "black";
+    //   ctx.fillRect(flower.x, flower.y, flower.width, flower.height);
+    // };
   });
 
   return (
