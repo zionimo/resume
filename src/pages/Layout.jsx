0fa42ad220a091066b09fca1c57e0c01b997e862
zionimo <zionimobu@gmail.com>
2023-06-26 22:47:52 +0900

    project 내용 수정

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,17 +1,57 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import Navbar from "../components/Navbar";
-
+import { HiOutlineArrowSmallUp } from "react-icons/hi2";
+import media from "../global/media";
+import { useRef } from "react";
 
 const Layout = () => {
+  const scrollContainerRef = useRef(null);
+
+  const goToTopHandle = () => {
+    scrollContainerRef.current.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div id="wrap">
-      <div id="body-container">
-        <Outlet />
+    <>
+      <div id="wrap" ref={scrollContainerRef}>
+        <div id="body-container">
+          <Outlet />
+        </div>
+
+        <Navbar />
       </div>
 
-      <Navbar />
-    </div>
+      <TopButton onClick={goToTopHandle}>
+        <HiOutlineArrowSmallUp />
+      </TopButton>
+    </>
   );
 };
 
 export default Layout;
+
+const TopButton = styled.div`
+  svg {
+    box-sizing: border-box;
+    width: 50px;
+    height: 50px;
+    padding: 8px;
+    border-radius: 50%;
+    background-color: #e0e0e075;
+    color: #0000009a;
+
+    position: fixed;
+    bottom: 1.5rem;
+    right: 6.5rem;
+  }
+
+  ${media.tablet`
+  svg{
+    right: 1.5rem;
+  }
+  `}
+`;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -68,39 +68,51 @@ const Projects = () => {
         "사용자가 원하는 사진, 문구를 이용하여 티셔츠 디자인을 커스터마이징 할 수 있는 서비스를 제공하는 사이트입니다.",
       duration: "2022.11.16 ~ 2023.01.06",
       durationDetail: "2022년 11월 16일 ~ 2023년 01월 06일",
-      language: "React, JavaScript, HTML, CSS ",
+      language: "React, JavaScript, HTML, CSS",
       link: "https://project-moti.netlify.app/",
       taskList: [
         {
           id: 1,
-          skill: "Redux",
+          skill: "React",
           context:
-            "Redux를 사용하여 전역상태를 중앙집중적으로 관리하여 효과적인 상태관리를 함 ",
+            "컴포넌트를 구성하고 재사용할 수 있으며, useState와 useEffect와 같은 React Hooks를 사용할 수 있습니다.",
         },
         {
           id: 2,
-          skill: "React-persist",
+          skill: "Redux",
           context:
-            "local storage를 활용하여 새로고침, 페이지 이동 등의 상황에도 리뷰 데이터가 유지될 수 있게 함",
+            "Redux를 사용하여 전역상태를 중앙집중적으로 관리하여 효과적인 상태관리를 하였습니다.",
         },
         {
           id: 3,
+          skill: "React-persist",
+          context:
+            "local storage를 활용하여 새로고침, 페이지 이동 등의 상황에도 리뷰 데이터가 유지될 수 있도록 하였습니다.",
+        },
+        {
+          id: 4,
           skill: "React-router",
           context:
-            "네비게이션 SPA(Single Page Application)방식을 통해 페이지간의 빠른 전환을 할 수 있으며 <Outlet/>을 사용하여 중첩된 라우트를 구현할 수 있음",
+            "네비게이션 SPA(Single Page Application)방식을 통해 페이지간의 빠른 전환을 할 수 있으며 <Outlet/>을 사용하여 중첩된 라우트를 구현할 수 있습니다.",
         },
 
         {
-          id: 4,
+          id: 5,
           skill: "React-slick",
           context:
-            "React-slick의 세팅값을 조절하여 메인페이지의 매끄러운 상하 스크롤 슬라이더를 구현함",
+            "React-slick의 세팅값을 조절하여 메인페이지의 매끄러운 상하 스크롤 슬라이더를 구현하였습니다.",
         },
         {
-          id: 5,
+          id: 6,
+          skill: "React-icons",
+          context:
+            "시각적인 요소를 추가하여 사용자가 편하게 사용할 수 있도록 하였습니다.",
+        },
+        {
+          id: 7,
           skill: "CSS",
           context:
-            "flex / grid를 통해 레이아웃 조작과 반응형 웹 애플리케이션을 구현할 수 있으며 반응형 메뉴리스트를 구현함",
+            "flex/grid를 통해 레이아웃 조작과 반응형 웹 애플리케이션을 구현할 수 있으며 반응형 메뉴리스트를 구현하였습니다. 미디어쿼리를 활용하여 반응형 웹사이트를 개발할 수 있습니다.",
         },
       ],
     },
@@ -111,40 +123,94 @@ const Projects = () => {
       name: "RESUME",
       title: "제가 궁금하신가요?",
       description:
-        "보고계시는 현재 사이트입니다. 저를 소개하기 위해 제작하였습니다.",
+        "보고계시는 현재 사이트입니다. 반응형 사이트로, 저를 소개하기 위해 제작되었습니다.",
       duration: "2023.03.20 ~ 2023.04.25",
       durationDetail: "2023년 03월 20일 ~ 2023년 04월 25일",
       language: "React, JavaScript, HTML, CSS, styled-components ",
-      link: "https://zionimo.github.io/resume/",
+
       taskList: [
         {
           id: 1,
-          skill: "React-router",
+          skill: "React",
           context:
-            "네비게이션 SPA(Single Page Application)방식을 통해 페이지간의 빠른 전환을 할 수 있으며 <Outlet/>을 사용하여 중첩된 라우트를 구현할 수 있음",
+            "컴포넌트를 구성하고 재사용할 수 있으며, useState와 useEffect와 같은 React Hooks를 사용할 수 있습니다.",
         },
         {
           id: 2,
-          skill: "React-slick",
+          skill: "React-router",
           context:
-            "프로젝트 리스트 나열에 필요한 세팅값을 조절하고 map() 함수를 함꼐 사용하여 데이터 배열을 순회하는 좌우 스크롤 슬라이더를 구현함",
+            "네비게이션 SPA(Single Page Application)방식을 통해 페이지간의 빠른 전환을 할 수 있으며 <Outlet/>을 사용하여 중첩된 라우트를 구현할 수 있습니다.",
         },
         {
           id: 3,
-          skill: "MUI",
+          skill: "React-slick",
           context:
-            "개발시간을 단축시킬 수 있고 세련되고 다양한 디자인을 제공하는 MUI 라이브러리를 커스터마이징하여 사용할 수 있음",
+            "프로젝트 리스트 나열에 필요한 세팅값을 조절하고 map() 함수를 함꼐 사용하여 데이터 배열을 순회하는 좌우 스크롤 슬라이더를 구현하였습니다.",
         },
         {
           id: 4,
+          skill: "MUI",
+          context:
+            "개발시간을 단축시킬 수 있고 세련되고 다양한 디자인을 제공하는 MUI 라이브러리를 커스터마이징하여 사용할 수 있습니다.",
+        },
+        {
+          id: 65,
+          skill: "React-icons",
+          context:
+            "시각적인 요소를 추가하여 사용자가 편하게 사용할 수 있도록 하였습니다.",
+        },
+        {
+          id: 6,
           skill: "styled-componets",
           context:
-            "컴포넌트 단위의 재사용 가능한 스타일링과 스타일 충돌방지에 이점을 느껴 사용함. CSS-in-JS 방식이므로 큰 규모의 프로젝트에서는 속도와 성능에 영향을 미칠 수 있음을 인지하고 프로젝트 스타일에 따라 적절히 사용할 예정",
+            "컴포넌트 단위의 재사용 가능한 스타일링과 스타일 충돌방지에 이점을 느껴 사용하였습니다. CSS-in-JS 방식이므로 큰 규모의 프로젝트에서는 속도와 성능에 영향을 미칠 수 있음을 인지하고 프로젝트 스타일에 따라 적절히 사용할 예정입니다.",
+        },
+        {
+          id: 7,
+          skill: "CSS",
+          context:
+            "flex/grid를 통해 레이아웃 조작과 반응형 웹 애플리케이션을 구현할 수 있으며 반응형 메뉴리스트를 구현하였습니다. 미디어쿼리를 활용하여 반응형 웹사이트를 개발할 수 있습니다.",
         },
       ],
     },
     {
       id: 3,
+      type: "개인 프로젝트",
+      img: require("../img/concert.png"),
+      name: "CONCERT FOR YOU",
+      title: "좋아하는 가수의 콘서트를 예약하세요",
+      description:
+        "Next.js와 TypeScript, TailwindCSS를 익히기 위해 제작하였습니다. 반응형으로 제작되었습니다.",
+      duration: "2023.06.13 ~",
+      durationDetail: "2023년 06월 13일 ~",
+      language: "Next.js, TypeScript, TailwindCSS, Framer Motion",
+      link: "https://concert-for-you.vercel.app/",
+      taskList: [
+        {
+          id: 1,
+          skill: "Next.js",
+          context:
+            "내장된 라우팅 기능을 사용하여 페이지 간의 이동을 구현할 수 있습니다.",
+        },
+        {
+          id: 2,
+          skill: "TypeScript",
+          context: "",
+        },
+        {
+          id: 3,
+          skill: "Framer Motion",
+          context: "",
+        },
+        {
+          id: 4,
+          skill: "TailwindCSS",
+          context: "",
+        },
+      ],
+    },
+    {
+      id: 4,
       type: "개인 프로젝트",
       img: require("../img/bookggureomi.jpg"),
       name: "BOOK GGUREOMI",
@@ -158,9 +224,11 @@ const Projects = () => {
       taskList: [],
     },
   ];
+  const lastProject = projectList.length;
 
   const goToDetail = (projectId) => {
-    if (projectId < 3) {
+    // 마지막 프로젝트는 준비중이므로 navigate연결 x
+    if (projectId < lastProject) {
       navigate(`/projects/${projectId}`, { state: projectList });
     }
   };
@@ -177,10 +245,10 @@ const Projects = () => {
             <div key={arr.id}>
               <ImageWrapper
                 onClick={() => goToDetail(arr.id)}
-                style={{ cursor: arr.id < 3 ? "pointer" : "default" }}
+                style={{ cursor: arr.id < lastProject ? "pointer" : "default" }}
               >
                 <Image src={arr.img} alt="index" />
-                {arr.id < 3 ? (
+                {arr.id < lastProject ? (
                   <ImageText>
                     <span>{arr.name}</span>
                     <span>{arr.duration}</span>
@@ -204,7 +272,7 @@ const Projects = () => {
               </ContextWrapper>
 
               {/* 제작중인 사이트는 버튼 노출되지 않음 */}
-              {arr.id < 3 ? (
+              {arr.id < lastProject ? (
                 <Button>
                   <Link to={"/projects/" + arr.id} state={projectList}>
                     More View
@@ -227,10 +295,10 @@ const SliderWrapper = styled.div`
   height: 100%;
   overflow: hidden;
   padding: 50px 1rem 100px 1rem;
-  
+
   border-top: 2px solid ${theme.SubTitle};
   border-bottom: 2px solid ${theme.SubTitle};
-  
+
   .slick-slide {
     box-sizing: border-box;
     /* 아이템 간 간격 조절 */
@@ -247,7 +315,7 @@ const SliderWrapper = styled.div`
     padding: 0;
   }
   `}
-  `;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
